8aeb4a1a8e79e119370064d6cd80ab4af4a89a5f
sourov-hossain1 <sourovhossain2122006@gmail.com>
2023-05-18 18:00:28 -0700

    banner section added

--- src/Home/Body/Body.jsx
+++ src/Home/Body/Body.jsx
@@ -1,8 +1,15 @@
-
+import './Body.css'
 const Body = () => {
     return (
-        <div>
-            <h2>body section</h2>
+        <div className='banner-section'>
+            <div className='left-banner'>
+                <h2 className='banner-title'>A New Box of <br /> Toys Every Month</h2> 
+                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure reprehenderit inventore sequi maiores ipsam, nam ratione itaque hic adipisci. Molestias?</h4>
+                <button className='bg-primary text-white p-2 rounded-md mt-4'>Get Your Toys</button>
+            </div>
+            <div className='right-banner'>
+                <img src="https://c1-ebgames.eb-cdn.com.au/merchandising/images/packshots/e0ac67d423a749c48a43a178d4e0fdae_Large.jpg" alt="" />
+            </div>
         </div>
     );
 };
--- src/Home/Navbar/Navbar.jsx
+++ src/Home/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
+// <div className="navbar bg-base-100">
+
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-lime-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
+                        <Link to='/'>Home</Link>
                         <li><a>All Toys</a></li>
                         <li><a>My Toys</a></li>
                         <li><a>Add Toy</a></li>
@@ -20,7 +22,7 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
+                    <Link to='/'>Home</Link>
                     <li><a>All Toys</a></li>
                     <li><a>My Toys</a></li>
                     <li><a>Add Toy</a></li>
